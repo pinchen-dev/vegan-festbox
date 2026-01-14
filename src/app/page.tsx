ce3d200405c084563b1c40634bf2ca753baabf90
PIN\a5190 <a5190279@gmail.com>
2026-01-14 22:45:11 +0800

    feat: add customer testimonials section and custom icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MaxWidthWrapper from "@/components/ui/MaxWidthWrapper"
 import Image from "next/image";
 import { Check, Phone, Star } from "lucide-react";
 import Box from "@/components/ui/Box"
+import { Icons } from "@/components/ui/Icons";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
               <div className="absolute w-32 left-0 -top-20 hidden lg:block">
                 <img src="/veganbox-1.png" className="w-full" />
               </div>
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold leading-tight! text-grey-900 text-5xl md:text-6xl lg:text-7xl">您的專屬 <span className="bg-primary px-2 text-white">純素生活</span> 定制提案
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold leading-tight! text-gray-900 text-5xl md:text-6xl lg:text-7xl">您的專屬 <span className="bg-primary px-2 text-white">純素生活</span> 定制提案
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
               「不只是美味。從零殘忍居家用品到純素零食，我們為您<span className="font-semibold">量身打造</span>每次的驚喜。
@@ -102,7 +103,48 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+        </section>
+        { /* value proposition section */}
+        <section className="bg-slate-100 py-24">
+          <MaxWidthWrapper className="flex flex-col items-center gap-16
+          sm:gap-32">
+            <div className="flex flex-col lg:flex-row items-center gap-4
+            sm:gap-6">
+              <h2 className="order-1 mt-2 tracking-tight text-center
+              text-balance leading-tight! font-bold text-5xl md:text-6xl
+              text-gray-900">
+                What our <span className="relative
+                px-2">customers<Icons.underline className="hidden sm:block
+                pointer-events-none absolute inset-x-0 -bottom-6
+                text-green-500"/>
+                </span> {''}say
+              </h2>
+              <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+            </div>
+
+            <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0
+            lg:max-w-none lg:grid-cols-2 gap-y-16">
+              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                    "The case feels durable and I even got a compliment on The
+                    design. Had the case for two and a half months now and
+                    <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had 
+                    before, the image started fading into yellow-ish color after a 
+                    couple weeks. Love it."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </MaxWidthWrapper>
       </section>
     </div>
-  );
+  )
 }
