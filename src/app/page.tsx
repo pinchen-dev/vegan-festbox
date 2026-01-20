a135cfb5d36c174291cbb08487b1b15ae1eb2345
PIN\a5190 <a5190279@gmail.com>
2026-01-20 22:41:09 +0800

    feat: complete customer testimonials and photo comparison section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import MaxWidthWrapper from "@/components/ui/MaxWidthWrapper"
+import MaxWidthWrapper from "@/components/ui/MaxWidthWrapper";
 import Image from "next/image";
 import { Check, Phone, Star } from "lucide-react";
-import Box from "@/components/ui/Box"
+import Box from "@/components/ui/Box";
 import { Icons } from "@/components/ui/Icons";
 import { Reviews } from "@/components/ui/Reviews";
 
@@ -15,196 +15,296 @@ export default function Home() {
               <div className="absolute w-32 left-0 -top-20 hidden lg:block">
                 <img src="/veganbox-1.png" className="w-full" />
               </div>
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold leading-tight! text-gray-900 text-5xl md:text-6xl lg:text-7xl">您的專屬 <span className="bg-primary px-2 text-white">純素生活</span> 定制提案
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold leading-tight! text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                您的專屬{" "}
+                <span className="bg-primary px-2 text-white">純素生活</span>{" "}
+                定制提案
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
-              「不只是美味。從零殘忍居家用品到純素零食，我們為您<span className="font-semibold">量身打造</span>每次的驚喜。
-              <span className="font-semibold">Vegan Festbox 友善動物、更友善地球環境</span>。加入我們，讓您每次開箱都像在拆一份精心準備的禮物。」<span className="font-semibold"></span>
+                「不只是美味。從零殘忍居家用品到純素零食，我們為您
+                <span className="font-semibold">量身打造</span>每次的驚喜。
+                <span className="font-semibold">
+                  Vegan Festbox 友善動物、更友善地球環境
+                </span>
+                。加入我們，讓您每次開箱都像在拆一份精心準備的禮物。」
+                <span className="font-semibold"></span>
               </p>
 
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
                     100% 全植物成分與零殘忍認證
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
                     嚴選在地小農與永續品牌
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
                     多樣化的純素零食與居家生活用品
                   </li>
                 </div>
               </ul>
               <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
                 <div className="flex -space-x-4">
-                  <img 
-                  className="inline-block h-10 w-10 rounded-full ring-2
-                   ring-slate-100" 
-                  src="/users/user-1.png"
-                  alt="user image"
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2
+                   ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user image"
                   />
-                  <img 
-                  className="inline-block h-10 w-10 rounded-full ring-2
-                   ring-slate-100" 
-                  src="/users/user-2.png"
-                  alt="user image"
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2
+                   ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user image"
                   />
-                  <img 
-                  className="inline-block h-10 w-10 rounded-full ring-2
-                   ring-slate-100" 
-                  src="/users/user-3.png"
-                  alt="user image"
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2
+                   ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user image"
                   />
-                  <img 
-                  className="inline-block h-10 w-10 rounded-full ring-2
-                   ring-slate-100" 
-                  src="/users/user-4.jpg"
-                  alt="user image"
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2
+                   ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="user image"
                   />
-                  <img 
-                  className="inline-block object-cover h-10 w-10 rounded-full ring-2
-                   ring-slate-100" 
-                  src="/users/user-5.jpg"
-                  alt="user image"
+                  <img
+                    className="inline-block object-cover h-10 w-10 rounded-full ring-2
+                   ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="user image"
                   />
                 </div>
 
                 <div className="flex flex-col justify-between items-center sm:items-start">
                   <div className="flex gap-0.5">
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600"/>
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600"/>
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
                   </div>
-                  <p><span className="font-semibold">1.250</span> happy customers</p>
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="col-span-full lg:col-span-1 w-full flex 
+          <div
+            className="col-span-full lg:col-span-1 w-full flex 
           justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 
-          h-fit">
+          h-fit"
+          >
             <div className="relative md:max-w-xl">
-              <img src="/your-image.png" className="absolute w-40 lg:w-52
+              <img
+                src="/your-image.png"
+                className="absolute w-40 lg:w-52
               left-56 -top-20 select-none hidden sm:block lg:hidden 
-              xl:block"/>
-              <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 
-              select-none"/>
+              xl:block"
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 
+              select-none"
+              />
               <Box className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
-        </section>
-        { /* value proposition section */}
-        <section className="bg-slate-100 py-24">
-          <MaxWidthWrapper className="flex flex-col items-center gap-16
-          sm:gap-32">
-            <div className="flex flex-col lg:flex-row items-center gap-4
-            sm:gap-6">
-              <h2 className="order-1 mt-2 tracking-tight text-center
+      </section>
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper
+          className="flex flex-col items-center gap-16
+          sm:gap-32"
+        >
+          <div
+            className="flex flex-col lg:flex-row items-center gap-4
+            sm:gap-6"
+          >
+            <h2
+              className="order-1 mt-2 tracking-tight text-center
               text-balance leading-tight! font-bold text-5xl md:text-6xl
-              text-gray-900">
-                What our <span className="relative
-                px-2">customers<Icons.underline className="hidden sm:block
+              text-gray-900"
+            >
+              What our{" "}
+              <span
+                className="relative
+                px-2"
+              >
+                customers
+                <Icons.underline
+                  className="hidden sm:block
                 pointer-events-none absolute inset-x-0 -bottom-6
-                text-green-500"/>
-                </span> {''}say
-              </h2>
-              <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
-            </div>
+                text-green-500"
+                />
+              </span>{" "}
+              {""}say
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
 
-            <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0
-            lg:max-w-none lg:grid-cols-2 gap-y-16">
-              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
-                <div className="flex gap-0.5 mb-2">
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                </div>
-                <div className="text-lg leading-8">
-                  <p>
-                    "The case feels durable and I even got a compliment on The
-                    design. Had the case for two and a half months now and
-                    <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had 
-                    before, the image started fading into yellow-ish color after a 
-                    couple weeks. Love it."
-                  </p>
-                </div>
-                <div className="flex gap-4 mt-2">
-                  <img
+          <div
+            className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0
+            lg:max-w-none lg:grid-cols-2 gap-y-16"
+          >
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on The
+                  design. Had the case for two and a half months now and
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple weeks. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
                   className="rounded-full h-12 w-12 object-cover"
                   src="/users/user-1.png"
                   alt="user"
-                  />
-                  <div className="flex flex-col">
-                    <p className="font-semibold">Jonathan</p>
-                    <div className="flex gap-1.5 items-center
-                     text-zinc-600">
-                      <Check className="h-4 w-4 stroke-[3px]
-                       text-green-600" />
-                      <p className="text-sm">Verified Purchase</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {/* second user review */}
-              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
-                <div className="flex gap-0.5 mb-2">
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                </div>
-                <div className="text-lg leading-8">
-                  <p>
-                    "I usually keep my phone together with my keys in my pocket 
-                    and that led to some pretty heavy scratchmarks on all of my
-                    last phone cases. The one, besides a barely noticeable
-                    scratch on the corner,{' '}
-                    <span className="p-0.5 bg-slate-800 text-white">
-                      looks brand new after about half a year
-                      </span>
-                      . I dig it."
-                  </p>
-                </div>
-                <div className="flex gap-4 mt-2">
-                  <img
-                  className="rounded-full h-12 w-12 object-cover"
-                  src="/users/user-4.jpg"
-                  alt="user"
-                  />
-                  <div className="flex flex-col">
-                    <p className="font-semibold">Josh</p>
-                    <div className="flex gap-1.5 items-center
-                     text-zinc-600">
-                      <Check className="h-4 w-4 stroke-[3px]
-                       text-green-600" />
-                      <p className="text-sm">Verified Purchase</p>
-                    </div>
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div
+                    className="flex gap-1.5 items-center
+                     text-zinc-600"
+                  >
+                    <Check
+                      className="h-4 w-4 stroke-[3px]
+                       text-green-600"
+                    />
+                    <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
               </div>
             </div>
-          </MaxWidthWrapper>
 
-          <div className="pt-16">
-            <Reviews />
+            {/* second user review */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone cases. The one, besides a barely noticeable scratch
+                  on the corner,{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Josh</p>
+                  <div
+                    className="flex gap-1.5 items-center
+                     text-zinc-600"
+                  >
+                    <Check
+                      className="h-4 w-4 stroke-[3px]
+                       text-green-600"
+                    />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+        </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2
+                className="order-1 mt-2 tracking-tight text-center
+              text-balance leading-tight! font-bold text-5xl md:text-6xl
+              text-gray-900"
+              >
+                Upload your photo and get {""}
+                <span className="relative px-2 bg-green-600 text-white">
+                  your own cases
+                </span>{" "}
+                {""}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div
+              className="relative flex flex-col items-center md:grid
+            grid-cols-2 gap-40"
+            >
+              <img
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2
+              z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+
+              <div
+                className="relative h-80 md:h-full w-full
+               md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset
+               ring-gray-900/10 lg:rounded-2xl"
+              >
+                <img
+                  src="/horse.jpg"
+                  className="rounded-md object-cover
+                bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
       </section>
     </div>
-  )
+  );
 }
