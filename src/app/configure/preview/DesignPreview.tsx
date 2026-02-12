79c20afaaa88c34146fe1e1e8e4b428aef3364ea
PIN\a5190 <a5190279@gmail.com>
2026-02-12 23:31:41 +0800

    feat: update db schema for orders and implement stripe checkout session

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -8,22 +8,45 @@ import Confetti from "react-dom-confetti";
 import { cn, formatPrice } from "@/lib/utils";
 import { ArrowRight, Check } from "lucide-react";
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
-import { Button } from "@headlessui/react";
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { createCheckoutSession } from "./action";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-const DesignPreview = ({ configuration }: {configuration: Configuration }) => {
+const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+  const router = useRouter() 
+
   const [showConfetti, setShowConfetti] = useState(false);
   useEffect(() => setShowConfetti(true));
 
-  const { color, model, finish, material } = configuration
-  const tw = COLORS.find((supportedColor) => 
-  supportedColor.value === color)?.tw
+  const { color, model, finish, material } = configuration;
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === color,
+  )?.tw;
 
-  const { label: modelLabel } = MODELS.options.find(({value}) => value === model)!
+  const { label: modelLabel } = MODELS.options.find(
+    ({ value }) => value === model,
+  )!;
 
-  let totalPrice = BASE_PRICE
+  let totalPrice = BASE_PRICE;
   if (material === "polycarbonate")
-    totalPrice += PRODUCT_PRICES.material.polycarbonate
-   if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured
+    totalPrice += PRODUCT_PRICES.material.polycarbonate;
+  if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
+
+const { mutate: createPaymentSession } = useMutation({
+  mutationKey: ["get-checkout-session"],
+  mutationFn: createCheckoutSession,
+  onSuccess: ({url}) => {
+    if (url) router.push(url)
+      else throw new Error("Unable to retrieve payment URL.")
+  },
+  onError: () => {
+        toast.error("Something went wrong", {
+          description: "There was an error on our end. Please try again.",
+        });
+      },
+})
 
   return (
     <>
@@ -38,11 +61,15 @@ const DesignPreview = ({ configuration }: {configuration: Configuration }) => {
         />
       </div>
 
-      <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12
-      sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+      <div
+        className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12
+      sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12"
+      >
         <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
-          <Box className={cn(`bg-${tw}`)}
-          imgSrc={configuration.croppedImageUrl!} />
+          <Box
+            className={cn(`bg-${tw}`)}
+            imgSrc={configuration.croppedImageUrl!}
+          />
         </div>
         <div className="mt-6 sm:col-sapn-9 sm:mt-0 md:row-end-1">
           <h3 className="text-3xl font-bold tracking-tight text-gray-900">
@@ -86,21 +113,21 @@ const DesignPreview = ({ configuration }: {configuration: Configuration }) => {
 
                 {finish === "textured" ? (
                   <div className="flex items-center justify-between py-1 mt-2">
-                  <p className="text-gray-600">Textured finish</p>
-                  <p className="font-medium text-gray-900">
-                    {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
-                  </p>
-                </div>
-                ): null}
+                    <p className="text-gray-600">Textured finish</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
+                    </p>
+                  </div>
+                ) : null}
 
                 {material === "polycarbonate" ? (
                   <div className="flex items-center justify-between py-1 mt-2">
-                  <p className="text-gray-600">Soft polycarbonate material</p>
-                  <p className="font-medium text-gray-900">
-                    {formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}
-                  </p>
-                </div>
-                ): null}
+                    <p className="text-gray-600">Soft polycarbonate material</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}
+                    </p>
+                  </div>
+                ) : null}
 
                 <div className="my-2 h-px bg-gray-200" />
 
@@ -115,7 +142,14 @@ const DesignPreview = ({ configuration }: {configuration: Configuration }) => {
             </div>
 
             <div className="mt-8 flex justify-end pb-12">
-              <Button>Check out </Button>
+              <Button
+              onClick={() => createPaymentSession({configId: configuration.id})}
+               disabled={true}
+                isLoading={false}
+                loadingText="loading"
+                className="px-4 sm:px-6 lg:px-8">
+                Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
+              </Button>
             </div>
           </div>
         </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,9 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { is } from "zod/v4/locales";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -33,30 +34,49 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
-)
+  },
+);
 
-function Button({
-  className,
-  variant = "default",
-  size = "default",
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
-  const Comp = asChild ? Slot : "button"
-
-  return (
-    <Comp
-      data-slot="button"
-      data-variant={variant}
-      data-size={size}
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
-  )
+export interface ButtonProps
+  extends
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  isLoading?: boolean;
+  loadingText?: string;
 }
 
-export { Button, buttonVariants }
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      isLoading,
+      loadingText,
+      variant,
+      children,
+      size,
+      asChild = false,
+      ...props
+    },
+    ref,
+  ) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {isLoading && loadingText ? loadingText : children}
+        {isLoading ? <span className="ml-1.5 flex items-center gap-1">
+          <span className="animate-flashing w-1 h-1 bg-white rounded-full inline-block" />
+          <span className="animate-flashing delay-100 w-1 h-1 bg-white rounded-full inline-block" />
+          <span className="animate-flashing delay-200 w-1 h-1 bg-white rounded-full inline-block" />
+          </span> : null}
+      </Comp>
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
