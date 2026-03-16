0ac189f62cec99db5cee39d583266491c1923cb1
PIN\a5190 <a5190279@gmail.com>
2026-03-16 10:21:16 +0800

    fix: resolve stripe api version mismatch with ts-ignore

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,7 @@
-import Stripe from "stripe"
+import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-    apiVersion: "2026-01-28.clover",
-    typescript: true,
-})
+  // @ts-ignore
+    apiVersion: "2026-02-25.clover",
+  typescript: true,
+});
